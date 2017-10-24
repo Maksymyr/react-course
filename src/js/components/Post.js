5e1431b3e888902537ae8785b931a2e0730c00c1
Maksymyr <maksymyr@gmail.com>
2017-10-24 23:20:40 +0300

    les2

--- src/js/components/Post.js
+++ src/js/components/Post.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 String.prototype.lessThan = function (max) {
     let tmp = this;
     for(let i = this.length; i >= max; i--) {
@@ -17,7 +18,14 @@ export default class Post extends React.Component {
     handleShowMore = () => {
         this.setState({ contentToggle: !this.state.contentToggle });
     };
-
+    handleDelete = () => {
+        this.props.delPost(this.props.index);
+    }
+    handleEdit = () => {
+        // console.log(this.props.index)
+        this.props.edPost(this.props.index, this.props.data);
+    }
+    
     contentView = (content) => {
         if(this.state.contentToggle && content.length > 120) {
             return content.lessThan(120) + '... ';
@@ -27,18 +35,19 @@ export default class Post extends React.Component {
     };
 
     render() {
+
         return (
             <article className={this.state.contentToggle ? "item" : "item active"}>
                 <h1>{this.props.data.title}</h1>
-                <p>{this.contentView(this.props.data.description)}</p>
+                <p>{this.contentView(this.props.data.description).replace(/[\s{2,}]+/g, ' ')}</p>
                 <ul className="links">
                     {(this.props.data.links) ? this.props.data.links.map((item, index) => <li key={index} className="link"><a href={item.link} target="_blank">{item.title}</a></li>) : null}
                 </ul>
                 
                 <div className="buttons">
                     <button onClick={this.handleShowMore}>{this.state.contentToggle ? "Show more" : "Show less"}</button>
-                    <button>Delete</button>
-                    <button>Edit</button>
+                    <button onClick={this.handleDelete}>Delete</button>
+                    <button onClick={this.handleEdit}>Edit</button>
                 </div>
 
             </article>
@@ -46,3 +55,21 @@ export default class Post extends React.Component {
     }
 }
 
+//         return (
+//     <article className="item">
+//     <div className="add-post">
+//         <form>
+//             <input type="text" value={this.props.data.title}/>
+//             <textarea>
+//             {this.props.data.description}
+//             </textarea>
+//         </form>
+//     </div>
+// <ul className="links">
+//         {(this.props.data.links) ? this.props.data.links.map((item, index) => <li key={index} className="link"><a href={item.link} target="_blank">{item.title}</a></li>) : null}
+//     </ul>
+// <div className="buttons">
+//     <button onClick={this.handleSave}>Save</button>
+// </div>
+
+// </article>
--- src/js/components/Posts.js
+++ src/js/components/Posts.js
@@ -2,38 +2,57 @@ import React from 'react';
 
 import Post from '../components/Post';
 import AddPost from './AddPost';
+import EditPost from './EditPost.js';
 
 export default class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checkout: true,
+            checkEdit: false,
+            objData:{}
+        }
+        this.editPost = this.editPost.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        
+    }
+    editPost (i, obj) {
+        console.log("i "+i)
+        this.setState({objData :obj, checkEdit : true});
+    }
+    handleClose(){
+        this.setState({checkEdit : false});
+    }
 
     // lifecycle methods START!
-    componentWillMount() {
-        console.log("Component Will Mount | Posts.js")
-    }
+    // componentWillMount() {
+    //     console.log("Component Will Mount | Posts.js")
+    // }
 
-    componentDidMount() {
-        console.log("Component Did Mount | Posts.js")
-    }
+    // componentDidMount() {
+    //     console.log("Component Did Mount | Posts.js")
+    // }
 
-    componentWillUpdate() {
-        console.log("Component Will Update | Posts.js");
-    };
+    // componentWillUpdate() {
+    //     console.log("Component Will Update | Posts.js");
+    // };
 
-    shouldComponentUpdate() {
-        console.log("Should Component Update | Posts.js");
-        return true;
-    };
+    // shouldComponentUpdate() {
+    //     console.log("Should Component Update | Posts.js");
+    //     return true;
+    // };
 
-    componentDidUpdate() {
-        console.log("Component Did Update | Posts.js");
-    };
+    // componentDidUpdate() {
+    //     console.log("Component Did Update | Posts.js");
+    // };
 
-    componentWillUnmount() {
-        console.log("componentWillUnmount | Posts.js");
-    }
+    // componentWillUnmount() {
+    //     console.log("componentWillUnmount | Posts.js");
+    // }
 
-    componentWillReceiveProps(nextProps) {
-        console.log("Component Will Receive Props | Posts.js", nextProps);
-    }
+    // componentWillReceiveProps(nextProps) {
+    //     console.log("Component Will Receive Props | Posts.js", nextProps);
+    // }
     //lifecycle methods END!
 
     // Это не все функции жизненного цикла, остальные вы можете узнать 
@@ -42,8 +61,12 @@ export default class Posts extends React.Component {
     render() {
         return (
             <section className="posts-container">
+
+
                 <AddPost addPost={this.props.addPost} />
+                {this.state.checkEdit ? <EditPost data={this.state.objData} index={this.props.posts.indexOf(this.state.objData)} onClick = {this.props.handleUpdate} handleClose={this.handleClose}/> : null}
                 <div className="items">
+
                     {this.props.posts.map((item, index) => {
                         // Тут мы перебираем функцией .map() каждый объект из массива переданого в этот компонент и передаем
                         // каждый из них в новый компонент Post (экземпляр класса), который создается при каждой итерации функции map()
@@ -51,7 +74,7 @@ export default class Posts extends React.Component {
                         // Так-же, мы передаем свойство "key", оно необходимо ядру реакта для индетификации элементов которые
                         // созданы спомощью итерационных функций, в остальных случаях это делать нет необходимости.
                         return (
-                            <Post data={item} key={index} />
+                            <Post data={item} delPost={this.props.delPost} key={index} index={index} edPost={this.editPost} checkbox={this.checkout}/>
                         )
                     } )}
                 </div>
